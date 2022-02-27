ffacb49d73bbccb33c89f20dcfc4da08ae2a8a42
Wobins <70920248+Wobins@users.noreply.github.com>
2022-02-27 11:28:09 +0100

    add delete buttton features

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-let screen = document.querySelector('.screen h3')
+let screen = document.querySelector('.screen p')
 let buttons = document.querySelectorAll('.button')
-let equalSign = document.querySelector('.equalSign')
-let clearBtn = document.querySelector('.clearBtn')
+let equalSign = document.querySelector('.equal')
+let clearBtn = document.querySelector('.clear')
+let deleteBtn = document.querySelector('.delete')
 
 
 // const display = () => {
@@ -23,4 +24,15 @@ equalSign.addEventListener('click', () => {
 
 clearBtn.addEventListener('click', () => {
     screen.textContent = ''
+})
+
+deleteBtn.addEventListener('click', function () {
+    let displayed = screen.textContent;
+    let arr = displayed.split("");
+    //console.log(arr)
+    let i = arr.length - 1;
+    let newArr = arr.slice(0, i);
+    //console.log(newArr)
+    screen.textContent = "";
+    newArr.forEach(n => screen.append(n));
 })
